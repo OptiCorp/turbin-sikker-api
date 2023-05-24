e36461dcd343c93cf2ef3226ce67943983cc41c0
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-05-24 09:42:24 +0200

    fix: changing int to string on Form model & cont..

--- turbin.sikker.core/Model/Form.cs
+++ turbin.sikker.core/Model/Form.cs
@@ -3,13 +3,13 @@ namespace turbin.sikker.core.Model
 {
 	public class Form
 	{
-		public int Id { get; set; }
+		public string Id { get; set; }
 
 		public string Title { get; set; }
 
 		//public DateOnly CreatedDate { get; set; }
 
-		public int CreatedBy { get; set; }
+		public string created_by { get; set; }
 	}
 }
 
