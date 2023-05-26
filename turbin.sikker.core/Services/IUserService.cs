fe910a392185d4b7d7d78c9d6121aace77b1666e
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-05-26 13:26:22 +0200

    Feat: Adding an Interface for the UserService

---
+++ turbin.sikker.core/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using turbin.sikker.core.Model;
+
+namespace turbin.sikker.core.Services
+{
+	public interface IUserService
+	{
+		IEnumerable<User> GetUsers();
+		Task<User> GetUserById(string id);
+		Task UpdateUser(string id, User user);
+		Task CreateUser(User user);
+		Task DeleteUser(string id);
+	}
+}
+
