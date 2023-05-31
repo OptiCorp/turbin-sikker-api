75af3aeed97eafd7166cdc23f283d42d8cdbff04
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-05-31 12:37:12 +0200

    fix: refactor UserRoleExists bool

--- turbin.sikker.core/Services/UserRoleService.cs
+++ turbin.sikker.core/Services/UserRoleService.cs
@@ -33,7 +33,7 @@ namespace turbin.sikker.core.Services
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!UserExists(id))
+                if (!UserRoleExists(id))
                 {
                     throw new ArgumentException("User does not exist");
                 }
@@ -61,9 +61,9 @@ namespace turbin.sikker.core.Services
             await _context.SaveChangesAsync();
         }
 
-        public bool UserExists(string id)
+        public bool UserRoleExists(string id)
         {
-            return (_context.User?.Any(user => user.id == id)).GetValueOrDefault();
+            return (_context.User_Role?.Any(userRole => userRole.Id == id)).GetValueOrDefault();
         }
 
     }
