4ec82ef9b908595e1a0574d4aed46af9656b15a6
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-06-09 12:11:55 +0200

    fix: fixing user model

--- turbin.sikker.core/Model/User.cs
+++ turbin.sikker.core/Model/User.cs
@@ -1,18 +1,30 @@
-﻿namespace turbin.sikker.core.Model
+﻿using System.ComponentModel.DataAnnotations;
+namespace turbin.sikker.core.Model
 {
     public class User
     {
-        public string id { get; set; }
-        
-        public string role_id { get; set; }
+        public string Id { get; set; }
 
-        public string first_name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string UserRoleId { get; set; }
 
-        public string last_name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
 
-        public string email { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
 
-        public string username { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
 
     }
 }
