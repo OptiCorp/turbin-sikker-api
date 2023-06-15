6696eb0d875dedcc0701416e970b7268fd7f4248
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-06-15 09:52:36 +0200

    feat: adding punchservice and interface

---
+++ turbin.sikker.core/Services/IPunchService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using turbin.sikker.core.Model;
+
+namespace turbin.sikker.core.Services
+{
+    public interface IUploadService
+    {
+        Upload GetUploadById(string id);
+        void UpdateUpload(Upload upload);
+        void CreateUpload(Upload upload);
+        void DeleteUpload(string id);
+    }
+}
+
---
+++ turbin.sikker.core/Services/PunchService.cs
@@ -0,0 +1,51 @@
+﻿using turbin.sikker.core.Model;
+
+namespace turbin.sikker.core.Services
+{
+    public class UploadService : IUploadService
+    {
+        private readonly TurbinSikkerDbContext _context;
+
+        public UploadService(TurbinSikkerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Upload GetUploadById(string id)
+        {
+            return _context.Upload.FirstOrDefault(u => u.Id == id);
+        }
+
+        public void CreateUpload(Upload upload)
+        {
+            _context.Upload.Add(upload);
+            _context.SaveChanges();
+        }
+
+        public void UpdateUpload(Upload updatedUpload)
+        {
+            var upload = _context.Upload.FirstOrDefault(u => u.Id == updatedUpload.Id);
+
+            if (upload != null)
+            {
+                upload.PunchId = updatedUpload.PunchId;
+                upload.BlobRef = updatedUpload.BlobRef;
+
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteUpload(string id)
+        {
+            var upload = _context.Upload.FirstOrDefault(u => u.Id == id);
+
+            if (upload != null)
+            {
+                _context.Upload.Remove(upload);
+                _context.SaveChanges();
+            }
+        }
+
+    }
+
+}
