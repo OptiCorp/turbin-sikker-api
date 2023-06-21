d70dcb542bd4cf9460584860f517d5f69b50f267
malasen <malasenkan.mu@gmail.com>
2023-06-21 13:25:39 +0200

    fix: added custom alidatin adn errorhandligng for checklist endpoins

---
+++ turbin.sikker.core/Model/DTO/ChecklistDtos/ChecklistMultipleResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace turbin.sikker.core.Model.DTO.ChecklistDtos
+{
+    public class ChecklistMultipleResponseDto
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [EnumDataType(typeof(ChecklistStatus))]
+        public ChecklistStatus Status { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public User? User { get; set; }
+    }
+}
--- turbin.sikker.core/Services/IChecklistService.cs
+++ turbin.sikker.core/Services/IChecklistService.cs
@@ -6,12 +6,14 @@ namespace turbin.sikker.core.Services
 {
     public interface IChecklistService
     {
-        Checklist GetChecklistById(string id);
-        IEnumerable<Checklist> GetAllChecklists();
+        ChecklistResponseDto GetChecklistById(string id);
+        IEnumerable<ChecklistMultipleResponseDto> GetAllChecklists();
         IEnumerable<ChecklistViewNoUserDto> GetAllChecklistsByUserId(string userId);
         void UpdateChecklist(string id, ChecklistEditDto checklist);
         string CreateChecklist(ChecklistCreateDto checklist);
         void DeleteChecklist(string id);
+
+        bool checklistExists(IEnumerable<ChecklistMultipleResponseDto> checklists, string userId, string title);
     }
 }
 
