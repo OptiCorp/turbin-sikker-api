08904c0b6f24b2c4c078fb1061f766e93854be91
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-06-30 14:50:52 +0200

    feat: userrole validator

---
+++ turbin.sikker.core/Validation/UserRoleValidations/UserRoleCreateValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using turbin.sikker.core.Model.DTO;
+using turbin.sikker.core.Services;
+
+namespace turbin.sikker.core.Validation.UserRoleValidations
+{
+    public class UserRoleCreateValidator : AbstractValidator<UserRoleCreateDto>
+    {
+        private readonly IUserRoleService _userRoleService;
+
+        public UserRoleCreateValidator(IUserRoleService userRoleService)
+        {
+            _userRoleService = userRoleService;
+
+
+            RuleFor(userRole => userRole.Name).NotEmpty().NotNull()
+                .Must(BeUniqueUserRole).WithMessage("User role already exist.");
+
+        }
+        bool BeUniqueUserRole(string userRoleName)
+        {
+            var userRoles = _userRoleService.GetUserRoles();
+
+            return !userRoles.Any(userRole => userRole.Name.ToLower() == userRoleName.ToLower());
+        }
+    }
+}
+
---
+++ turbin.sikker.core/Validation/UserRoleValidations/UserRoleUpdateValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using turbin.sikker.core.Model.DTO;
+using turbin.sikker.core.Services;
+
+namespace turbin.sikker.core.Validation.UserRoleValidations
+{
+    public class UserRoleUpdateValidator : AbstractValidator<UserRoleUpdateDto>
+    {
+        private readonly IUserRoleService _userRoleService;
+
+        public UserRoleUpdateValidator(IUserRoleService userRoleService)
+        {
+            _userRoleService = userRoleService;
+
+
+            RuleFor(userRole => userRole.Name).NotEmpty().NotNull()
+                .Must(BeUniqueUserRole).WithMessage("User role name is taken.");
+
+        }
+        bool BeUniqueUserRole(string userRoleName)
+        {
+            var userRoles = _userRoleService.GetUserRoles();
+
+            return !userRoles.Any(userRole => userRole.Name.ToLower() == userRoleName.ToLower());
+        }
+    }
+}
+
