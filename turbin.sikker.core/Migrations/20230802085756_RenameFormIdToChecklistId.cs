bb17d3f16c6fe6d46225d931e89c64c8049a3013
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-08-03 15:04:33 +0200

    EF migrations

---
+++ turbin.sikker.core/Migrations/20230802085756_RenameFormIdToChecklistId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace turbin.sikker.core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameFormIdToChecklistId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FormId",
+                table: "Punch",
+                newName: "ChecklistId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ turbin.sikker.core/Migrations/20230803091636_Remove_FK_Punch_User_CreatedBy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace turbin.sikker.core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_FK_Punch_User_CreatedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE [Punch] DROP CONSTRAINT IF EXISTS [FK_Punch_User_CreatedBy];");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
