03ca4029841018b3decac9bbd0a69e028d7eb600
Henrik <henrik.laland@bouvet.no>
2023-08-23 13:30:36 +0200

    added auth

--- turbin.sikker.core/Startup.cs
+++ turbin.sikker.core/Startup.cs
@@ -99,14 +99,14 @@ namespace turbin.sikker.core
 
 
             // TODO: Implement Authorization
-            //services.AddAuthorization(options =>
-            //{
-            //    var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(
-            //        JwtBearerDefaults.AuthenticationScheme, "AzureAD"
-            //        );
-            //    defaultAuthorizationPolicyBuilder = defaultAuthorizationPolicyBuilder.RequireAuthenticatedUser();
-            //    options.DefaultPolicy = defaultAuthorizationPolicyBuilder.Build();
-            //});
+            services.AddAuthorization(options =>
+            {
+               var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(
+                   JwtBearerDefaults.AuthenticationScheme, "AzureAD"
+                   );
+               defaultAuthorizationPolicyBuilder = defaultAuthorizationPolicyBuilder.RequireAuthenticatedUser();
+               options.DefaultPolicy = defaultAuthorizationPolicyBuilder.Build();
+            });
 
             services.AddAuthentication().AddIdentityServerJwt();
 
