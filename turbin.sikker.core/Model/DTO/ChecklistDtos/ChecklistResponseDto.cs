5db74db11deced505490c1aec29ff28e35fa4e93
Eirik Bakkan <eirikbakkan@gmail.com>
2023-09-15 13:14:38 +0200

    fixed dto, removed required in response

--- turbin.sikker.core/Model/DTO/ChecklistDtos/ChecklistResponseDto.cs
+++ turbin.sikker.core/Model/DTO/ChecklistDtos/ChecklistResponseDto.cs
@@ -4,22 +4,17 @@ namespace turbin.sikker.core.Model.DTO.ChecklistDtos
 {
     public class ChecklistResponseDto
     {   
-        [Required]
         public string? Id { get; set; }
 
-        [Required]
         [StringLength(50)]
         public string? Title { get; set; }
 
-        [Required]
         public string? Status { get; set; }
 
-        [Required]
         public DateTime? CreatedDate { get; set; }
 
         public DateTime? UpdatedDate { get; set; }
 
-        [Required]
         public User? User { get; set; }
         public ICollection<ChecklistTask>? ChecklistTasks { get; set; }
     }
