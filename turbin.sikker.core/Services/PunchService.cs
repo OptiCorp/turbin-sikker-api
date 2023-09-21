0f2f76039a27f3138f0b8f0d97a64585654516ae
Eirik Bakkan <eirikbakkan@gmail.com>
2023-09-21 09:48:43 +0200

    include uploads in punches and delete uploads when deleting punches

--- turbin.sikker.core/Services/PunchService.cs
+++ turbin.sikker.core/Services/PunchService.cs
@@ -23,6 +23,7 @@ namespace turbin.sikker.core.Services
                             .Include(u => u.Uploads)
                             .Include(p => p.Creator)
                             .ThenInclude(u => u.UserRole)
+                            .Include(u => u.Uploads)
                             .OrderByDescending(c => c.CreatedDate)
                             .Select(p => _punchUtilities.PunchToResponseDto(p))
                             .ToListAsync();
@@ -35,6 +36,7 @@ namespace turbin.sikker.core.Services
                                 .Include(u => u.Uploads)
                                 .Include(p => p.Creator)
                                 .ThenInclude(u => u.UserRole)
+                                .Include(u => u.Uploads)
                                 .FirstOrDefaultAsync(p => p.Id == id);
 
             return _punchUtilities.PunchToResponseDto(punch);
@@ -52,6 +54,7 @@ namespace turbin.sikker.core.Services
                                     .Include(u => u.Uploads)
                                     .Include(p => p.Creator)
                                     .ThenInclude(u => u.UserRole)
+                                    .Include(u => u.Uploads)
                                     .OrderByDescending(c => c.CreatedDate)
                                     .Select(c => _punchUtilities.PunchToResponseDto(c))
                                     .ToListAsync();
@@ -69,6 +72,7 @@ namespace turbin.sikker.core.Services
                             .Include(p => p.Creator)
                             .ThenInclude(u => u.UserRole)
                             .Where(c => c.CreatorId == id)
+                            .Include(u => u.Uploads)
                             .OrderByDescending(c => c.CreatedDate)
                             .Select(c => _punchUtilities.PunchToResponseDto(c))
                             .ToListAsync();
@@ -82,6 +86,7 @@ namespace turbin.sikker.core.Services
                             .Include(p => p.Creator)
                             .ThenInclude(u => u.UserRole)
                             .Where(c => c.WorkflowId == id)
+                            .Include(u => u.Uploads)
                             .OrderByDescending(c => c.CreatedDate)
                             .Select(c => _punchUtilities.PunchToResponseDto(c))
                             .ToListAsync();
