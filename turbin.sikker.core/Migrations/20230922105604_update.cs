b1d4d0542f34c559f4a83bb5681ffd68e642937e
Eirik Bakkan <eirikbakkan@gmail.com>
2023-09-22 13:04:44 +0200

    db fix

---
+++ turbin.sikker.core/Migrations/20230922105604_update.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace turbin.sikker.core.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "HoursSpent",
+                table: "Workflow",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "AvgHoursSpent",
+                table: "Checklist",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HoursSpent",
+                table: "Workflow");
+
+            migrationBuilder.DropColumn(
+                name: "AvgHoursSpent",
+                table: "Checklist");
+        }
+    }
+}
