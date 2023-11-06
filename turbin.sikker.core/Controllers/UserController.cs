d9af14db2a610c59cd127d857ca443f2ccfefaf0
Eirik Bakkan <eirikbakkan@gmail.com>
2023-11-06 12:48:09 +0100

    added back user controllwe, but deleted create, update and delete endpoints

---
+++ turbin.sikker.core/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using turbin.sikker.core.Model;
+using Swashbuckle.AspNetCore.Annotations;
+using turbin.sikker.core.Services;
+using turbin.sikker.core.Model.DTO;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using turbin.sikker.core.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+
+namespace turbin.sikker.core.Controllers
+{
+    [Authorize(Policy = "AuthZPolicy")]
+    [EnableCors("AllowAllHeaders")]
+    [ApiController]
+    [Route("api")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IUserUtilities _userUtilities;
+
+        public UserController(IUserService userService, IUserUtilities userUtilities)
+        {
+            _userService = userService;
+            _userUtilities = userUtilities;
+        }
+
+        [HttpGet("GetAllUsers")]
+        [SwaggerOperation(Summary = "Get all users", Description = "Retrieves a list of all users.")]
+        [SwaggerResponse(200, "Success", typeof(IEnumerable<UserDto>))]
+
+        public async Task<IActionResult> GetAllUsersAsync()
+        {
+            return Ok(await _userService.GetAllUsersAsync());
+        }
+
+        [HttpGet("GetAllUsersAdmin")]
+        [SwaggerOperation(Summary = "Get all users", Description = "Retrieves a list of all users.")]
+        [SwaggerResponse(200, "Success", typeof(IEnumerable<UserDto>))]
+
+        public async Task<IActionResult> GetAllUsersAdminAsync()
+        {
+            return Ok(await _userService.GetAllUsersAdminAsync());
+        }
+
+        [HttpGet("GetUser")]
+        [SwaggerOperation(Summary = "Get user by ID", Description = "Retrieves a user by their ID.")]
+        [SwaggerResponse(200, "Success", typeof(UserDto))]
+        [SwaggerResponse(404, "User not found")]
+        public async Task<IActionResult> GetUserByIdAsync(string id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("GetUserByAzureAdUserId")]
+        [SwaggerOperation(Summary = "Get Azure AD user by ID", Description = "Retrieves a Azure AD user by their ID.")]
+        [SwaggerResponse(200, "Success", typeof(User))]
+        [SwaggerResponse(404, "Azure AD user not found")]
+        public async Task<IActionResult> GetUserByAzureAdUserIdAsync(string azureAdUserId)
+        {
+            var user = await _userService.GetUserByAzureAdUserIdAsync(azureAdUserId);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("GetUserByUserName")]
+        [SwaggerOperation(Summary = "Get user by username", Description = "Retrieves a user by their username.")]
+        [SwaggerResponse(200, "Success", typeof(UserDto))]
+        [SwaggerResponse(404, "User not found")]
+        public async Task<IActionResult> GetUserByUsernameAsync(string username)
+        {
+            var user = await _userService.GetUserByUsernameAsync(username);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            return Ok(user);
+        }
+    }
+}
